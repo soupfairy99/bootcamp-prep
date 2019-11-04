4f52e16baa6b956e092a74c63a004a9b8082a581
Lori K <soupfairy99@gmail.com>
2019-11-04 19:32:11 +0300

    solution

--- pre-work-workshops/06-problem-set/01-greeting/greeting.js
+++ pre-work-workshops/06-problem-set/01-greeting/greeting.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function greeting (name){
+    //returns greeting with name 
+    if (name){
+        return `Hello ${name}!`;
+    }
+    else {
+        return "Hello!"
+    }
+}
