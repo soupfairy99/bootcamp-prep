9bed7b7f6c763246d94ba5c32a9f13a9d94b5fc4
Lori K <soupfairy99@gmail.com>
2019-11-04 20:21:57 +0300

    solution

--- pre-work-workshops/06-problem-set/03-last-character/last-character.js
+++ pre-work-workshops/06-problem-set/03-last-character/last-character.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function lastCharacter (string1, string2){
+    //returns true if the last characters are equal
+    let string1LastChar = string1.charAt(string1.length -1);
+    let stringLastChar2 = string2.charAt(string2.length -1);
+    if (stringLastChar2 === string1LastChar) {
+        return true;
+    }
+    else {
+        return false; 
+    }
+}
