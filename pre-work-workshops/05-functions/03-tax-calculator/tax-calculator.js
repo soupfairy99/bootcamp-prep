e1f262d0daa3cd43c82c4346480ba21c39d14927
Lori K <soupfairy99@gmail.com>
2019-11-04 19:18:16 +0300

    solution

--- pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
+++ pre-work-workshops/05-functions/03-tax-calculator/tax-calculator.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function taxCalculator (price, state){
+    //calculate total price based on state tax rate
+    if (state === 'NJ') {
+        return price * 1.06625;
+    }
+    if (state === 'NY') {
+        return price * 1.04; 
+    }
+}
