8405441a837e136d055a3e187a4938385d8df84e
Lori K <soupfairy99@gmail.com>
2019-11-04 16:52:09 +0300

    solution

--- pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
+++ pre-work-workshops/04-control-flow/03-truth-teller/truth-teller.js
@@ -6,3 +6,12 @@ let boolean1 = true, boolean2 = true;
 let result;
 
 // YOUR CODE BELOW
+if (boolean1 && boolean2){
+    result = 'both';
+}
+else if (boolean1 || boolean2){
+    result = 'one';
+}
+else {
+    result = 'none';
+}
