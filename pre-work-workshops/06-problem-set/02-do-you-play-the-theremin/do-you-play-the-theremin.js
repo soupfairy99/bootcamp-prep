9b7e5ec2a327b4b8730c949847eacc1568556d4a
Lori K <soupfairy99@gmail.com>
2019-11-04 20:14:59 +0300

    solution

--- pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
+++ pre-work-workshops/06-problem-set/02-do-you-play-the-theremin/do-you-play-the-theremin.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function doYouPlayTheTheremin(string){
+    //returns true is the string begins with 's' or 'S'
+    let stringFirstChar = string.charAt(0);
+    if (stringFirstChar  === 's' || stringFirstChar ==='S') {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
