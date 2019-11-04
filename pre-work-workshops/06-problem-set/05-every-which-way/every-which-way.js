0200357c43542ecd1db376e0e8a3ec3bab0a5e12
Lori K <soupfairy99@gmail.com>
2019-11-04 20:39:33 +0300

    solution

--- pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
+++ pre-work-workshops/06-problem-set/05-every-which-way/every-which-way.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function everyWhichWay(num1, num2, num3){
+    //checks if num3 is a sum, difference, product or fraction of first 2 arguments
+    if (num3 === (num1 + num2)){
+        return 'sum';
+    }
+    else if (num3 === (num1 - num2)){
+        return 'difference';
+    }
+    else if (num3 === (num1 * num2)){
+        return 'product';
+    }
+    else if (num3 === (num1 / num2)){
+        return 'fraction';
+    }
+    else {
+        return null;
+    }
+}
