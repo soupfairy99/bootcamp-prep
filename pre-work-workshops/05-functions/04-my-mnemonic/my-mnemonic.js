e89fbde65c80233363517fa43a488a9a34ddeaa6
Lori K <soupfairy99@gmail.com>
2019-11-04 19:27:53 +0300

    solution

--- pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
+++ pre-work-workshops/05-functions/04-my-mnemonic/my-mnemonic.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myMnemonic () {
+    //return the first letter of each argument passed in 
+    let returnMnemonic = '';
+    for (let x = 0; x < arguments.length; x++) {
+        returnMnemonic += arguments[x].charAt(0);
+    }
+    return returnMnemonic;
+}
