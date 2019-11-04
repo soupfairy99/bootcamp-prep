264f8f96221e512819a1e336a3ff330338cb5fa6
Lori K <soupfairy99@gmail.com>
2019-11-04 19:05:27 +0300

    solution

--- pre-work-workshops/05-functions/02-default-greet/default-greet.js
+++ pre-work-workshops/05-functions/02-default-greet/default-greet.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function defaultGreet(firstName, lastName){
+    let returnString='';
+    if (!lastName) {
+        lastName = 'Doe'
+    }
+    
+  //  return "Hi " + firstName + " "+ lastName +"!";
+
+  return `Hi ${firstName} ${lastName}!`;
+}
