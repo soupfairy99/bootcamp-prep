3bcb96c52c97b6b5eaa97794e5606131cf3e82f0
Lori K <soupfairy99@gmail.com>
2019-11-04 20:31:13 +0300

    solution

--- pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
+++ pre-work-workshops/06-problem-set/04-max-of-three/max-of-three.js
@@ -1,5 +1,6 @@
 // YOUR CODE BELOW
 function maxOfThree (num1, num2, num3){
+    // returns the max of 3 numbers
     let max = num1;
     for (let x = 0; x < arguments.length;x++){
         if (max < arguments[x])
