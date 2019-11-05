8d0ee2fb650df7b71fe9763cfa5aaf531ec2053e
Lori K <soupfairy99@gmail.com>
2019-11-05 14:55:01 +0300

    solution

--- workshops/02-loops/02-crazy-caps/crazy-caps.js
+++ workshops/02-loops/02-crazy-caps/crazy-caps.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function crazyCaps (string){
+// capitalizes every other char
+  string.toLowerCase(); 
+  let crazyCapsString = "";
+  let i = 0;
+  //loop through string. Capitalize char at odd index
+  while (i < string.length){
+      if (i % 2 === 1) {
+          crazyCapsString += string[i].toUpperCase(); 
+      }
+      else {
+          crazyCapsString += string[i];
+      }
+      i++; 
+  }
+  return crazyCapsString; 
+}
