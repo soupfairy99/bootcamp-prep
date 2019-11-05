8212d189951a2d3552b3d17604cef16d6b23e59b
Lori K <soupfairy99@gmail.com>
2019-11-05 16:18:44 +0300

    solution

--- workshops/02-loops/05-my-slice/my-slice.js
+++ workshops/02-loops/05-my-slice/my-slice.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function mySlice (originalString, startIdx, endIdx){
+    //return a string copy starting at startIdx and ending before endIdx
+    let stringSliced = "";
+    // initialize start and end indexes if undefined
+    if (startIdx === undefined) {
+        startIdx = 0;
+    }
+    if (endIdx === undefined) {
+        endIdx = originalString.length; 
+    }
+    //loop and copy within start and end indices
+    for (let x =startIdx; x < endIdx; x++){
+        stringSliced += originalString[x];
+    }
+    return stringSliced;
+}
