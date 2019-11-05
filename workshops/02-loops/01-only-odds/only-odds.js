f2565db64b2c840644994795a803cb808a7c299d
Lori K <soupfairy99@gmail.com>
2019-11-05 14:44:33 +0300

    solution

--- workshops/02-loops/01-only-odds/only-odds.js
+++ workshops/02-loops/01-only-odds/only-odds.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function onlyOdds (someNumber){
+    //find the sum of all odd integers between 1 and the parameter
+    let oddSum = 0;
+    for (let i = 1; i <= someNumber; i++){
+        //loop from 1 to someNumber; test if odd
+        if (i % 2 === 1 ){
+      //if add, add to result
+          oddSum += i;     
+        }
+    }
+    return oddSum;
+}
