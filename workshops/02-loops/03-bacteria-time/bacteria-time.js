5ab3bb63f96092e8b34814080e46c59ea9b07cd0
Lori K <soupfairy99@gmail.com>
2019-11-05 15:13:36 +0300

    solution

--- workshops/02-loops/03-bacteria-time/bacteria-time.js
+++ workshops/02-loops/03-bacteria-time/bacteria-time.js
@@ -1,1 +1,14 @@
-// YOUR CODE BELOW
+// YOUR CODE BELOw
+function bacteriaTime (currentNum, targetNum){
+    //return number of minutes needed for currentNum to reach target num
+    let numberOfMinutes = 0;
+    if (targetNum < currentNum){
+        return 'targetNum must be larger than currentNum';
+    }
+    while (currentNum < targetNum) {
+        //currentNum doubles each time period of 20 minutes
+        currentNum = currentNum * 2;
+        numberOfMinutes += 20; 
+    }
+    return numberOfMinutes; 
+}
