a9552033ba5d8217fd80e1ade3ecc9d5250cfd35
Lori K <soupfairy99@gmail.com>
2019-11-05 16:03:29 +0300

    solution

--- workshops/02-loops/04-exponentiate/exponentiate.js
+++ workshops/02-loops/04-exponentiate/exponentiate.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function exponentiate (base, power){
+// raise the base to a power
+  if (power === 0){
+      return 1;
+  }
+  else {
+      return base * exponentiate(base, power-1);
+  }
+}
