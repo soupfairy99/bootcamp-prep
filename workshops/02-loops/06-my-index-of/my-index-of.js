117153ad80080c6027b7aea227ac6fe92e87e245
Lori K <soupfairy99@gmail.com>
2019-11-06 16:12:56 +0300

    solution

--- workshops/02-loops/06-my-index-of/my-index-of.js
+++ workshops/02-loops/06-my-index-of/my-index-of.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+
+function myIndexOf (source, searchValue, startIdx){
+//function to find the start index of the searchValue in the string
+//set startIdx to 0 if a value is not already assigned
+    if (!startIdx) {
+        startIdx = 0;
+    }
+//loop through string checking for searchValue
+    for (let i = startIdx; i < source.length; i++){
+        if (searchValue[0] === source[i]) {
+//if first char found, take a slice as long as searchValue and compare
+            let sourceVal = source.slice(i,searchValue.length + i);
+            if (sourceVal === searchValue){
+                return i;            
+            }
+        }
+    }
+    return -1; 
+}
