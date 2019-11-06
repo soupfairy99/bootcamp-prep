a204997c454df0f35b91a84e6bbb4a491bb73e21
Lori K <soupfairy99@gmail.com>
2019-11-06 19:26:34 +0300

    solution

--- workshops/02-loops/07-most-vowels/most-vowels.js
+++ workshops/02-loops/07-most-vowels/most-vowels.js
@@ -1,1 +1,36 @@
 // YOUR CODE BELOW
+function mostVowels (stringOfWords) {
+//returns word with the most vowels. First split string into an array of words
+    let array = stringOfWords.split(' '); 
+    let max = 0;
+    let index = 0;
+    let count = 0;
+    let vowels = 'aeiouAEIOU';
+    let char = '';
+//loop through array items
+        for (let i = 0; i < array.length; i++){
+            let word = array[i];
+            count = 0;
+//loop through characters in each word checking for vowels
+            for (let y = 0; y < word.length; y++) {
+                char = word[y];
+                if ( vowels.includes(char)){
+                    count++
+                }
+            } 
+//check for max vowels
+            if (count > max){
+                max = count;
+                index = i;
+            }
+        }
+   if (max === 0) {
+       return '';
+   }
+   let returnword = array[index];
+//check for '.' if most vowels word is the last word
+   if (returnword[returnword.length-1]==='.'){
+       return returnword.slice(0,-1);
+   }
+   return returnword; 
+}
