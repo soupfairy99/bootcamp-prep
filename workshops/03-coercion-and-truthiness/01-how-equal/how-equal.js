8891b19299bf72775eabade97316134ad8c87145
Lori K <soupfairy99@gmail.com>
2019-11-06 19:43:36 +0300

    solution

--- workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
+++ workshops/03-coercion-and-truthiness/01-how-equal/how-equal.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function howEqual(argument1, argument2){
+    if (argument1 === argument2){
+        return 'strictly';
+    }
+    else if (argument1 == argument2){
+        return 'loosely';
+    }
+    else{
+        return 'not equal';
+    }
+}
