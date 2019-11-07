15827f6bcc63906d02caf0416c0f6780cb598280
Lori K <soupfairy99@gmail.com>
2019-11-07 15:26:20 +0300

    solution

--- workshops/03-coercion-and-truthiness/04-only-one/only-one.js
+++ workshops/03-coercion-and-truthiness/04-only-one/only-one.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function onlyOne (arg1, arg2, arg3){
+    //return true only if one and only one argument is true
+    if (!!arg1 && !arg2 && !arg3) {
+        return true;
+    }
+    if (!arg1 && !!arg2 && !arg3){
+        return true;
+    }
+    if (!arg1 && !arg2 && !!arg3){
+        return true;
+    }
+    return false;
+}
