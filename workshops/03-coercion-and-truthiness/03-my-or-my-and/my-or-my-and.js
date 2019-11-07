886a57b08334b5d99109e9746dba780cb49bb5ab
Lori K <soupfairy99@gmail.com>
2019-11-07 14:28:17 +0300

    solution

--- workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
+++ workshops/03-coercion-and-truthiness/03-my-or-my-and/my-or-my-and.js
@@ -1,1 +1,25 @@
 // YOUR CODE BELOW
+function myOr(arg1, arg2, arg3){
+//tests the truthiness of each consecutive value
+    if (!!arg1){
+        return arg1;
+    }
+    else if (!!arg2){
+        return arg2;
+    }
+    else {
+        return arg3;
+    }
+}
+
+function myAnd (arg1, arg2, arg3){
+//tests the falseyness of each consecutive value
+
+    if (!arg1){
+        return arg1;
+    }
+    else if (!arg2){
+        return arg2;
+    }
+    else {return arg3;}
+}
