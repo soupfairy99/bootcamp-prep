96d14178dfa7732bd1865c18039de9f7b4f26298
Lori K <soupfairy99@gmail.com>
2019-11-07 15:47:23 +0300

    solution

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+function zeroDarkThirty (num){
+//remove the zeros from a number
+    if (num === 0){
+        return NaN; 
+    }
+//convert the number to a string, and then to an array
+    let numArray = num.toString().split('');
+//loop through the array, removing zeros
+    for (let i = 0; i < numArray.length; i++){
+        let currVal =numArray[i];
+        if (currVal === 0){
+            numArray.splice(i,1);
+        }
+    }
+//convert the array to a string, then to a float
+    return parseFloat(numArray.join());
+}
