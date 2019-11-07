128bf6280e8db5844383a97ff6236fbbe57238e5
Lori K <soupfairy99@gmail.com>
2019-11-07 14:07:27 +0300

    solution

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,26 @@
 // YOUR CODE BELOW
+
+function isTruthy (anyArgument) {
+//check if argument is any condition of truthy
+    if (!!anyArgument){
+        return true;
+    }
+//check for various conditions of falsey
+    else {
+        if (anyArgument === false){
+            return 'The boolean value false is falsey';
+        }
+        if (anyArgument === null){
+            return 'The null value is falsey';
+        }
+        if (anyArgument === undefined){
+            return 'undefined is falsey'
+        }
+        if (anyArgument === 0) {
+            return 'The number 0 is falsey (the only falsey number)';
+        }
+        if (anyArgument ===''){
+            return 'The empty string is falsey (the only falsey string)';
+        }
+    }
+}
