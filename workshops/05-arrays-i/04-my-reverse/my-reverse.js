93e7b03226e8b41b4a9639b2454b4ec1a1297fd5
Lori K <soupfairy99@gmail.com>
2019-11-10 16:12:58 +0300

    solution

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myReverse (array){
+//return reversed array
+    let reverseArray = [];
+    for (let i = 0; i < array.length; ++i){
+        reverseArray.unshift(array[i]);
+    }
+    return reverseArray; 
+}
