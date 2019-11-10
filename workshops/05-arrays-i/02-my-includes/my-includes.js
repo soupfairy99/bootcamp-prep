884f7e6c3593bcd8b824b0176feca5d33f2e15ee
Lori K <soupfairy99@gmail.com>
2019-11-10 15:23:10 +0300

    solution

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function myIncludes (array, searchVal){
+//check for a searchValue in an array and return true if found
+    for (let i = 0; i < array.length; ++i ){
+        if (array[i] === searchVal){
+            return true;
+        }
+    }
+    return false; 
+}
