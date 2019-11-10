62366ef80f4446e24201ba0c91e6a08e2242dfa4
Lori K <soupfairy99@gmail.com>
2019-11-10 16:03:50 +0300

    solution

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function myLastIndexOf (array, searchVal, startIdx){
+//return last index of search value from array
+    if (!startIdx){
+        startIdx = array.length-1;
+    }
+//loop through array decrementing until value found
+    for (let i = startIdx; i >= 0; --i){
+        if (array[i] === searchVal){
+            return i;
+        }
+    }
+return -1; 
+}
