91a358decf2d90cbb44459c64f1155856ebe4af5
Lori K <soupfairy99@gmail.com>
2019-11-10 16:21:39 +0300

    solution

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+function myUnshift (arr, value){
+//returns a new array with the value as the first index
+    let newArray = [value];
+    for (let i = 0; i < arr.length; ++i){
+        newArray.push(arr[i]);
+    }
+    return newArray;
+}
