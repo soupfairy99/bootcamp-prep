0d0b69d7907303ba73c448e54873c2c97835b5b3
Lori K <soupfairy99@gmail.com>
2019-11-10 15:14:56 +0300

    solution

--- workshops/05-arrays-i/01-odd-couple/odd-couple.js
+++ workshops/05-arrays-i/01-odd-couple/odd-couple.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function oddCouple (numArray){
+
+//return a new array with two or fewer odd numbers
+    let oddCoupleArray = [];
+    let oddCount = 0;
+    for (let x = 0; x < numArray.length; ++x){
+//write odd numbers to the new array until count is 2
+        if (numArray[x] % 2 === 1 && oddCount < 2) {
+            oddCoupleArray.push(numArray[x]);
+            oddCount++;
+        }
+    }
+    return oddCoupleArray;
+}
