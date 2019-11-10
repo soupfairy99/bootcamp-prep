551da6caa4e958ed1883ed20f6d8c4a5ad566aec
Lori K <soupfairy99@gmail.com>
2019-11-10 16:51:36 +0300

    solution

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+function evenAndOdd (array){
+//return a new array containing an array with even numbers followed by an array of odds
+    let bothArray = [[],[]];
+    for (let i = 0; i < array.length; ++i ){
+        if (array[i] % 2 ===  1){
+            bothArray[1].push(array[i]);
+        }
+        else {
+            bothArray[0].push(array[i]);
+        }
+    }
+    return bothArray;
+}
