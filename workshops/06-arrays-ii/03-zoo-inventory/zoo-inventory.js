1f927a6f42ed8c7b7e63ed4e40bbbc63c48e531d
Lori K <soupfairy99@gmail.com>
2019-11-11 13:39:43 +0300

    solution

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function zooInventory(myZoo){
+//return a flat array with sentences about each array element animal
+    let sentenceZoo = [];
+    for (let i = 0; i < myZoo.length; ++i){
+        let currAnimal = myZoo[i];
+//extract data from array and set to variable names
+        let name = currAnimal[0];
+        let species = currAnimal[1][0];
+        let age = currAnimal[1][1];
+//create sentences with string literals and push to output array
+        sentenceZoo.push(`${name} the ${species} is ${age}.`);
+    }
+    return sentenceZoo; 
+}
