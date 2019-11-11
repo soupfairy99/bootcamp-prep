5580934ccb97ee410448038514818775dad3e16d
Lori K <soupfairy99@gmail.com>
2019-11-11 14:31:52 +0300

    solution

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,19 @@
 // YOUR CODE BELOW
+function myJoin(array, separator){
+    if (separator === undefined){
+        separator = ',';
+    }
+    let string ='';
+    let tempString ='';
+    for (let i = 0; i < array.length-1; ++i){
+        if ( !array[i]){
+             tempString ='';
+        }
+        else{
+         tempString = array[i]; 
+        }
+        string += tempString + separator;
+    }
+    string += array[array.length -1];
+    return string;
+}
