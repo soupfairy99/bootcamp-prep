3638090bcca22fb553f10b4b9e379a7a412eb81b
Lori K <soupfairy99@gmail.com>
2019-11-11 13:53:31 +0300

    solution

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+
+function makeGrid(numColumns, numRows){
+//create a two dimensional array with numbers
+    let gridArray =[];
+    let rowArray = [];
+//make row arrays with numbers
+    for (let i = 1; i <= numColumns; ++i){
+        rowArray.push(i);
+    }
+//push rows to outer array
+    for (let j = 0; j < numRows; j++){
+        gridArray.push(rowArray);
+    }
+    return gridArray; 
+}
