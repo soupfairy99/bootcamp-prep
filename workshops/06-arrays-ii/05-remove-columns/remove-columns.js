3324af83a4b59d2d4cebc48a4ab69a692dc80e93
Lori K <soupfairy99@gmail.com>
2019-11-11 14:05:39 +0300

    message

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,9 @@
 // YOUR CODE BELOW
+
+function removeColumns(originalGrid, numColums){
+//remove the correct number of elements from each inner array
+    for (let i = 0; i < originalGrid.length; ++i){
+        originalGrid[i].splice(numColums*-1);
+    }
+return originalGrid;
+}
