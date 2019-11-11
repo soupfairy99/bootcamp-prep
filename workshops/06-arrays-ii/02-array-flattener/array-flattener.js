6166b31bfe163072399ccd9d76fb4964b9e27fdf
Lori K <soupfairy99@gmail.com>
2019-11-11 13:26:15 +0300

    solution

--- workshops/06-arrays-ii/02-array-flattener/array-flattener.js
+++ workshops/06-arrays-ii/02-array-flattener/array-flattener.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function arrayFlattener(twoDArray){
+//write a two dimensional array to one dimensional array
+    let flatArray =[];
+    for (let i = 0; i < twoDArray.length; ++i){
+//check for inner array and concat instead of push
+        if (Array.isArray(twoDArray[i])){
+           flatArray = flatArray.concat(twoDArray[i]);
+        }
+        else{
+            flatArray.push(twoDArray[i]);
+        }
+    }
+    return flatArray;
+}
