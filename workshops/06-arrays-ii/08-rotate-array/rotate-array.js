7df52a4faaa1f8d895fdfe39ba275ce813d8c2fb
Lori K <soupfairy99@gmail.com>
2019-11-13 16:21:21 +0300

    solution

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,18 @@
 // YOUR CODE BELOW
+
+function rotateArray (originalArray, rotateNum){
+    let newArray= [];
+    let spliceArray = []; 
+    if (rotateNum === 0){
+        newArray = originalArray;
+    }
+    if (Math.sign(rotateNum)=== -1){
+        spliceArray = originalArray.splice(0,Math.abs(rotateNum));
+        newArray = originalArray.concat(spliceArray);
+    }
+    else {
+        spliceArray= originalArray.splice(rotateNum * -1);
+        newArray = spliceArray.concat(originalArray);
+    }
+    return newArray; 
+}
