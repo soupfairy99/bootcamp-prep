889cc24c26a5171e8694d4c9c7e1a91bf43c7116
Lori K <soupfairy99@gmail.com>
2019-11-13 13:04:16 +0300

    solution

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,24 @@
 // YOUR CODE BELOW
+
+function mySlice (originalArray, startIdx, endIdx){
+//mimic the slice method
+    let newArray = [];
+//assign undefined variables
+    if (!startIdx) {
+        startIdx = 0;
+    }
+    if (!endIdx){
+        endIdx = originalArray.length;
+    }
+//re-assign negative variables
+    if (Math.sign(startIdx)=== -1){
+        startIdx = originalArray.length + startIdx
+    }
+    if (Math.sign(endIdx)=== -1){
+        endIdx = originalArray.length + endIdx
+    }
+    for (let i = startIdx; i < endIdx; ++i){
+        newArray.push(originalArray[i])
+    }
+return newArray;
+}
