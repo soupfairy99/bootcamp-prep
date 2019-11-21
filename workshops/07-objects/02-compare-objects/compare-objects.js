44b06aa067eb7f0e986024fff16be3668fb211a4
Lori K <soupfairy99@gmail.com>
2019-11-21 18:34:06 +0300

    solution

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+const compareObjects = (obj1, obj2) => {
+//compare two objects. return true if identical, false if not
+    for (let key in obj1){
+        if (obj1[key] !== obj2[key]) {
+            return false;
+        }
+    }
+    for (let prop in obj2){
+        if (obj2[prop]!== obj1[prop]){
+            return false;
+        }
+    }
+return true; 
+}
