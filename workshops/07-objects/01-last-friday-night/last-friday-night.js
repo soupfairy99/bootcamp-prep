8aad22af63175b3abdb36dfb555ac6a0885d368d
Lori K <soupfairy99@gmail.com>
2019-11-21 17:48:37 +0300

    solution

--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+
+const lastFridayNight = (transactions) => {
+    //returns the total of amounts within each object in the array
+    let returnVal = 0;
+    for (let x = 0; x < transactions.length; x++){
+        returnVal += transactions[x].amount;
+    }
+    return returnVal; 
+}
