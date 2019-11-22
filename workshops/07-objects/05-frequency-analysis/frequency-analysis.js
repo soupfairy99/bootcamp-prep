cd208b92ab9851cab707f76a9bf602b92b03d529
Lori K <soupfairy99@gmail.com>
2019-11-22 10:44:50 +0300

    solution

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+//function to count the occurence of each character in a string
+const frequencyAnalysis = (stringLc) => {
+    let alphaCountObj = {};
+    for (let i = 0; i < stringLc.length; ++i){
+//if object does not yet contain the char, init count (value) to one for that property
+        if (!alphaCountObj[stringLc[i]]){
+            alphaCountObj[stringLc[i]] = 1;
+        }
+//if prop exits, increment by one
+        else{
+            alphaCountObj[stringLc[i]]++;
+        }
+    }
+    return alphaCountObj;
+}
