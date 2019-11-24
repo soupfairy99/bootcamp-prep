d91ea1b2f532356143b753b07b9935c152ef9441
Lori K <soupfairy99@gmail.com>
2019-11-24 12:13:25 +0300

    solution

--- workshops/07-objects/07-attendance-check/attendance-check.js
+++ workshops/07-objects/07-attendance-check/attendance-check.js
@@ -38,3 +38,21 @@ let classRoom = [
 ];
 
 // YOUR CODE BELOW
+const attendanceCheck = (dayOfWeek) => {
+//check gobal classRoom array for dayOfWeek parameter. Return array of keys 
+    studentPresentArray =[];
+    for (let x = 0; x < classRoom.length; ++x){
+//set current student attendance record object to currStudent
+        let currStudent = classRoom[x]; 
+//set current student name to variable to possibly push to returned array
+        let studentName = Object.keys(currStudent);
+//loop through array of day objects to check for the day of week
+        let currWeek = currStudent[studentName];
+        for (let y = 0; y < currWeek.length; y++){
+            if (currWeek[y][dayOfWeek]){
+                studentPresentArray.push(studentName.toString());
+            }
+        }
+    }
+return studentPresentArray; 
+}
