775db5a534cd7ba3cca78970b7beae8ee6137080
Lori K <soupfairy99@gmail.com>
2019-11-25 14:09:24 +0300

    solution

--- workshops/08-object-methods/02-say-your-name/say-your-name.js
+++ workshops/08-object-methods/02-say-your-name/say-your-name.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+
+const me2 = {
+    name: 'Fred',
+    getGreeting: function (){
+        let friend = {
+            name: 'Jeff'
+        }
+        return  `Hi ${friend.name}, my name is ${me2.name}.`
+    }
+
+}
