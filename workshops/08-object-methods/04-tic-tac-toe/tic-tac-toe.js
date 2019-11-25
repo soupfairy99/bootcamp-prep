fa9b5dcd97aad61585d67da01924a1f41befa09a
Lori K <soupfairy99@gmail.com>
2019-11-25 16:15:16 +0300

    solution

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,22 @@
 // YOUR CODE BELOW
+let ticTacToe = {
+//initialize board
+    board: [[null,null,null],[null,null,null],[null,null,null]],
+//place 'x' or 'o' if space not taken
+    move: function(char, rowNum, colNum)  {
+        
+        if (this.board[rowNum][colNum] === null){
+            this.board[rowNum][colNum] = char; 
+        }
+        console.log(this.board);
+        return this.board; 
+    },
+//re=initialize board
+    clear: function(){
+        for (let x = 0; x < this.board.length; x++){
+            this.board[x] = [null,null,null];
+        }
+        console.log(this.board);
+        return this.board;
+    }
+}
