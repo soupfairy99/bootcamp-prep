64665fac80efb64a0154ff1595ca9098953a51bc
Lori K <soupfairy99@gmail.com>
2019-11-25 14:37:21 +0300

    solution

--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+const callThemAll =(obj, num) => {
+    let returnedVal = [];
+    for (let key in obj) {
+        if (typeof obj[key] === 'function'){       
+        returnedVal.push(obj[key](num));
+        }  
+    } 
+    return returnedVal; 
+}
